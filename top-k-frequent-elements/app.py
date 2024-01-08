d84ba3f9b9577baecefc1e0fc85dfb46ad664189
KaranbirSingh7 <karanbirsingh1161@gmail.com>
2024-01-08 00:39:09 -0500

    Update app.py

--- top-k-frequent-elements/app.py
+++ top-k-frequent-elements/app.py
@@ -1,27 +1,31 @@
 from typing import List
 
 # problem: https://leetcode.com/problems/top-k-frequent-elements/description/
+# solution: using bucket sort
+#          xref: https://www.youtube.com/watch?v=YPTqKIgVk-k
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = {}
-
-        # sort list
-        nums = sorted(nums)
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
 
         for n in nums:
-            # increment counter
-            if n in res:
-                res[n] = res[n] + 1
-            # else initiate count
-            else:
-                res[n] = 1
+            count[n] = 1 + count.get(n, 0)
+        # would result in {1: 3, 2: 2, 3:1, 4: 3 ....}
 
-        # loop over dict values and send back the anything more than 1
-        new_list = [key for key,value in res.items() if value >= k]
-        return new_list        
+        for n, c in count.items():
+            freq[c].append(n)
+        # would result in [ 0[] 1[3] 2[2] 3[1,4] ]
+
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+     
 
 nums = [1,1,1,2,2,3]
-k = 2
+k = 1
 solution = Solution()
 solution.topKFrequent(nums, k)
